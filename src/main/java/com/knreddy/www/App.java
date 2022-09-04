5f7542f62b8bc5a9a81e5edbaddb33f7297911dc
knreddy <knreddy3391@gmail.com>
2022-09-04 13:05:56 +0100

    starter project

---
+++ src/main/java/com/knreddy/www/App.java
@@ -0,0 +1,18 @@
+package com.knreddy.www;
+
+/**
+ * Hello world!
+ *
+ */
+ public class App 
+{
+    public static void main(String[] args){
+    int age=20;  
+    if(age>18){  
+        System.out.println("Age is greater than 18");  
+    }
+    else{
+        System.out.println("My age is less thatn 18");
+    }
+    }
+}  
